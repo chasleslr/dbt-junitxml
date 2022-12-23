1856bfa3d8d53931c467010480cf69673a809647
Aliaksandra Sidarenka <96114873+si-aliaksandra@users.noreply.github.com>
2022-12-23 11:52:14 +0100

    Add log, sql to the report portal, change total testsuites time (#1)
    * [AS] add log to the report, change total testsuites time
    
    * add sql
    
    * update sql text
    
    Co-authored-by: BUDAPEST\Aliaksandra_Sidarenk <aliaksandra_sidarenka@epam.com>

--- src/dbt_junitxml/main.py
+++ src/dbt_junitxml/main.py
@@ -1,11 +1,19 @@
 import click
 import json
 
-from junit_xml import TestCase, TestSuite, to_xml_report_string
+from junit_xml import to_xml_report_string
+from dbt_junitxml.dbt_junit_xml import DBTTestSuite, DBTTestCase
+from datetime import datetime
+import os
 
 
 class InvalidRunResult(Exception):
     pass
+
+
+def convert_timestamp_to_isoformat(timestamp: str) -> str:
+    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ').strftime(
+        '%Y-%m-%dT%H:%M:%S')
 
 
 @click.group()
@@ -19,12 +27,19 @@ def cli():
     type=click.Path(exists=True)
 )
 @click.argument(
+    "manifest",
+    type=click.Path(exists=True)
+)
+@click.argument(
     "output",
     type=click.Path(exists=False)
 )
-def parse(run_result, output):
+def parse(run_result, manifest, output):
     with open(run_result) as f:
         run_result = json.load(f)
+
+    with open(manifest) as m:
+        manifest = json.load(m)['nodes']
 
     try:
         rpc_method = run_result["args"]["rpc_method"]
@@ -34,34 +49,69 @@ def parse(run_result, output):
             raise InvalidRunResult("run_result.json other than v4 are not supported.")
 
         if not rpc_method == "test":
-            raise InvalidRunResult(f"run_result.json must be from the output of `dbt test`. Got dbt {rpc_method}.")
+            raise InvalidRunResult(
+                f"run_result.json must be from the output of `dbt test`. Got dbt {rpc_method}.")
 
     except KeyError as e:
         raise InvalidRunResult(e)
 
     tests = run_result["results"]
+    total_elapsed_time = run_result["elapsed_time"]
+    test_suite_timestamp = convert_timestamp_to_isoformat(run_result["metadata"]["generated_at"])
+
+    tests_manifest = {}
+    for key, config in manifest.items():
+        if config['resource_type'] == 'test':
+            test_name = key.split('.')[2]
+            tests_manifest[test_name] = config
+            sql_path = os.path.join(config['root_path'], 'target', 'compiled', config['package_name'],
+                                    config['original_file_path'], config['path'])
+            sql_log = \
+                f"""select * from {tests_manifest[test_name]['schema']}.{tests_manifest[test_name]['alias']
+                if tests_manifest[test_name]['alias'] else tests_manifest[test_name]['name']}"""
+            sql_log_format = "\n" + '-'*96 + "\n" + sql_log + "\n" + '-'*96
+            try:
+                with open(sql_path, 'r') as sql:
+                    sql_text = sql.readlines()
+                    sql_text.insert(0, sql_log_format)
+                    tests_manifest[test_name]['sql'] = str.join('', sql_text)
+            except FileNotFoundError as e:
+                sql_text = config['compiled_sql'] if 'compiled_sql' in config.keys() else config[
+                    'raw_sql']
+                sql_text = [sql_log_format, sql_text]
+                tests_manifest[test_name]['sql'] = str.join('', sql_text)
+
 
     test_cases = []
     for test in tests:
-        test_case = TestCase(
+        test_name = test["unique_id"].split('.')[2]
+        test_timestamp = test['timing'][0]["started_at"] if ["status"] == 'pass' \
+            else test_suite_timestamp
+        test_sql = tests_manifest[test_name]["sql"] if test_name in tests_manifest.keys() else 'N/A'
+        test_case = DBTTestCase(
             classname=test["unique_id"],
             name=test["unique_id"].split(".")[2],
             elapsed_sec=test["execution_time"],
             status=test["status"],
+            timestamp=test_timestamp,
+            stdout=test_sql
         )
 
         if test["status"] == "fail":
-            test_case.add_failure_info(message=test["message"])
+            test_case.add_failure_info(message=test["message"], output=test["message"])
 
         if test["status"] == "error":
-            test_case.add_error_info(message=test["message"])
+            test_case.add_error_info(message=test["message"], output=test["message"])
 
         if test["status"] == "skipped":
-            test_case.add_skipped_info(message=test["message"])
+            test_case.add_skipped_info(message=test["message"], output=test["message"])
 
         test_cases.append(test_case)
 
-    test_suite = TestSuite("Tests", test_cases=test_cases)
+    test_suite = DBTTestSuite(f"Tests",
+                              test_cases=test_cases,
+                              time=total_elapsed_time,
+                              timestamp=test_suite_timestamp)
 
     xml_report = to_xml_report_string([test_suite])
 
